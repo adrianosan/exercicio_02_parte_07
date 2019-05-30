950eb318c3e29ce6b356d92ab4b1342d6299517d
adriano.santos <adriano.santos@squadra.com.br>
2019-05-30 16:56:27 -0300

    exercicios 02 parte 07

---
+++ exercicio_ 02 parte 07/exercicio_ 02 parte 07/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio__02_parte_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d;
+            int media;
+
+            Console.WriteLine("Digite a primeira nota. ");
+
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a segunda nota. ");
+
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a terceira nota. ");
+
+            c = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a quarta nota. ");
+
+            d = int.Parse(Console.ReadLine());
+
+            media = (a + b + c + d) / 4;
+
+            if (media < 6)
+            {
+
+                Console.WriteLine("Reprovado!");
+            }
+            else
+            {
+                Console.WriteLine("Aprovado!");
+            }
+            Console.ReadKey();
+        }
+            }
+                }
+
+                    
+                
+           
+               
+         
+
+          
+
+          
+
+
+                   
+                      
+
+                       
+               
+            
+          
+            
+
+        
+    
